c96244be418173829ae7dc78309d7b29f7f6ebfa
wagatsuma.yuichi <firefly.of.april@gmail.com>
2025-03-04 09:52:31 +0000

    映画レコードをJSON形式で表示できるようにした

--- section003/watchmate/watchlist_app/urls.py
+++ section003/watchmate/watchlist_app/urls.py
@@ -1,1 +1,6 @@
-# モデル定義
+from django.urls import path
+from watchlist_app.views import movie_list
+
+urlpatterns = [
+    path("list/", movie_list, name="movie-list"),
+]
--- section003/watchmate/watchlist_app/views.py
+++ section003/watchmate/watchlist_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from watchlist_app.models import Movie
+from django.http import JsonResponse
 
-# Create your views here.
+
+def movie_list(request):
+    movies = Movie.objects.all()
+
+    # moviesをJson形式で返すためには、QuerySet型のため、辞書型に変換した後でリスト型に変換する
+    data = {"movies": list(movies.values())}
+    return JsonResponse(data)
